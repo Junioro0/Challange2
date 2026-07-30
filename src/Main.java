95e37ee9b8c927fe5c5004026797138cb8f4cf3d
Ronaldo Júnior <junioronaldolima@gmail.com>
2026-07-30 15:15:58 -0300

    inicio do codigo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("digite a mensagem: ");
+        String mensagem = scanner.nextLine();
+
+
+        }
+    }
