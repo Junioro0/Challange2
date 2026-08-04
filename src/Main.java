7156ef0b9f152583b5ce23470e4ed01f8217889f
Ronaldo Júnior <junioronaldolima@gmail.com>
2026-08-04 14:37:20 -0300

    Criação do if e else

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("digite a mensagem: ");
-        String mensagem = scanner.nextLine();
+        System.out.println("Pede senha ou código? (sim/não)");
+        String resposta = scanner.nextLine();
 
+        if (resposta.equalsIgnoreCase("sim")) {
+            System.out.println("Mensagem suspeita ");
 
+        } else {
+            System.out.println("Mensagem provavelmente legitima ");
+
+            }
         }
     }
